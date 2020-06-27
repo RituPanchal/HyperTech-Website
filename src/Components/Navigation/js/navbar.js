0725df9ad9f75bb964c85ae02841de7fa3b8e1f1
RituPanchal <rpanchal1899@gmail.com>
2020-06-27 19:23:56 +0100

    re-setup

--- src/Components/Navigation/js/navbar.js
+++
@@ -1,9 +0,0 @@
-import $ from "jquery";
-
-$(document).ready(function () {
-    $(".nav-link span").click(function(event) {
-        $('.nav-link span').removeClass("active");
-        $(this).addClass("active");
-    });
-    
-    });
